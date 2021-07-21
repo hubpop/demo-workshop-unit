d46ce43b997b82622f90ac8a09b1983fd7d1b30b
Hubert Popielarz <hubert.x.popielarz@gsk.com>
2021-07-21 11:55:55 +0200

    Fixed tests

--- src/test/java/com/gsk/demoworkshopunits/controller/UserControllerTest.java
+++ src/test/java/com/gsk/demoworkshopunits/controller/UserControllerTest.java
@@ -4,20 +4,21 @@ import com.google.gson.Gson;
 import com.gsk.demoworkshopunits.model.User;
 import com.gsk.demoworkshopunits.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -53,10 +54,39 @@ class UserControllerTest {
     }
 
     @Test
-    void getById() {
+    void getById() throws Exception {
+        //given
+        userRepository.add(new User("Joe", "Doe"));
+
+        //when
+        MvcResult mvcResult = this.mockMvc.perform(get("/user/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        User returnedUser = gson.fromJson(mvcResult.getResponse().getContentAsString(), User.class);
+
+        //then
+        Assertions.assertEquals("Joe", returnedUser.getName());
+        Assertions.assertEquals("Doe", returnedUser.getLastName());
     }
 
     @Test
-    void addUser() {
+    void addUser() throws Exception {
+        //given
+        User newUser = new User("John", "Dole");
+
+        //when
+        MvcResult mvcResult = this.mockMvc.perform(post("/user/add")
+                .content(gson.toJson(newUser))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String returnedId = mvcResult.getResponse().getContentAsString();
+
+        //then
+        Assertions.assertEquals("2", returnedId);
     }
 }
