0867e079bda8f09eeb3e6fac1b264d3bb5a62881
Hubert Popielarz <hubert.x.popielarz@gsk.com>
2021-07-21 12:22:47 +0200

    deleted @Data annotation to avoid unit testing

--- src/main/java/com/gsk/demoworkshopunits/model/User.java
+++ src/main/java/com/gsk/demoworkshopunits/model/User.java
@@ -1,8 +1,10 @@
 package com.gsk.demoworkshopunits.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class User {
     private String name;
     private String lastName;
