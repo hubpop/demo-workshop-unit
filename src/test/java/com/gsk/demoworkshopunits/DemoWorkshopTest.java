1668255f3ba6dfa099321c0ff941c69e38c1f4fe
Hubert Popielarz <hubert.x.popielarz@gsk.com>
2021-07-22 16:45:20 +0200

    Stub test for main method - only for better code coverage

---
+++ src/test/java/com/gsk/demoworkshopunits/DemoWorkshopTest.java
@@ -0,0 +1,13 @@
+package com.gsk.demoworkshopunits;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoWorkshopTest {
+
+    @Test
+    void main() {
+        DemoWorkshop.main(new String[] {});
+    }
+}
